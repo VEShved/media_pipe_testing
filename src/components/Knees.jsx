b0e0e45b699e7de0d142ea856c2138a9f69db7f8
VEShved <v.shved@evrone.com>
2022-03-06 14:39:48 +0200

    cleanup code

--- src/components/Knees.jsx
+++ src/components/Knees.jsx
@@ -1,6 +1,31 @@
 import { POSE_LANDMARKS_LEFT, POSE_LANDMARKS_RIGHT } from '@mediapipe/pose';
 import Knee from './Knee';
 
+const kneePresent = (knee) => knee.hip && knee.knee && knee.ankle
+
+function printToConsole (left, right) {
+  if (kneePresent(left)) {
+    console.log(`Left: ${Knee({ landmarks: left })}`);
+  };
+
+  if (kneePresent(right)) {
+    console.log(`Right: ${Knee({ landmarks: right })}`);
+  };
+
+  return "";
+};
+
+function addDiv (left, right) {
+  return (
+    <div>
+      {kneePresent(left) ? <p>
+        Left: <Knee landmarks={left} />
+      </p> : ""}
+      {kneePresent(right) ? <p> Right: <Knee landmarks={right} /> </p> : ""}
+    </div>
+  );
+};
+
 export default function Knees ({ landmarks }) {
   const leftLandmarks = {
     hip: landmarks[POSE_LANDMARKS_LEFT.LEFT_HIP],
@@ -13,12 +38,9 @@ export default function Knees ({ landmarks }) {
     ankle: landmarks[POSE_LANDMARKS_RIGHT.RIGHT_ANKLE]
   };
 
-  const kneePresent = (knee) => knee.hip && knee.knee && knee.ankle
+  // Print knee angles to console
+  return printToConsole(leftLandmarks, rightLandmarks)
 
-  return (
-    <div>
-      {kneePresent(leftLandmarks) ? <p> Left: <Knee landmarks={leftLandmarks} /> </p> : ""}
-      {kneePresent(rightLandmarks) ? <p> Right: <Knee landmarks={rightLandmarks} /> </p> : ""}
-    </div>
-  );
+  // Print knee angles under the video
+  // return addDiv(leftLandmarks, rightLandmarks)
 };
